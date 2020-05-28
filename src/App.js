55af047886b4bb1a2d62da8c1bbb847c4bef6384
AvocadoCasualty <baibeebambi@gmail.com>
2020-05-28 10:27:09 -0700

    Notes complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './Components/Header';
-import Landing from './Components/Landing';
+import router from "./router";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Landing />
+        {router}
     </div>
   );
 }
